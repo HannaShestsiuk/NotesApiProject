c0e9f48bac683f1241445212032b648cbf0cd209
Hanna.Shestsiuk <hannashestsiuk@gmail.com>
2026-02-10 00:00:28 +0100

    fix: update Login page

--- src/main/java/pages/practicePages/LoginPage.java
+++ src/main/java/pages/practicePages/LoginPage.java
@@ -2,9 +2,15 @@ package pages.practicePages;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import helpers.VisibleLocator;
 import io.qameta.allure.Step;
 import pages.BasePage;
 import records.practiceRecords.PracticeUiUser;
+
+import static constants.Constants.LOGIN_PAGE;
+import static constants.Constants.SECURE_PAGE;
+import static constants.Messages.*;
 
 public class LoginPage extends BasePage {
     public LoginPage(Page page) {
@@ -13,54 +19,76 @@ public class LoginPage extends BasePage {
 
     @Override
     protected String path() {
-        return "/login";
+        return LOGIN_PAGE;
     }
 
-    public boolean isAt() {
-        return page.url().endsWith("/login");
-    }
-
-    private Locator usernameField() {
+    private Locator usernameInput() {
         return page.locator("#username");
     }
 
-    private Locator passwordField() {
+    private Locator passwordInput() {
         return page.locator("#password");
     }
 
     private Locator loginButton() {
-        return page.locator("button:has-text('Login')");
+        return page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Login")
+        );
     }
 
-    public Locator flashMessage() {
-        return page.locator("#flash-message b");
+    public VisibleLocator flashMessage() {
+        return should(page.locator("#flash-message b"));
     }
 
-    public String getFlashMessage() {
-        return flashMessage().textContent().trim();
+    public Locator successRegisteredMessage = page.getByText(SUCCESSFUL_REGISTRATION);
+
+    public Locator invalidUserNameMessage = page.getByText(LOGIN_INVALID_USERNAME);
+
+    public Locator invalidPasswordMessage = page.getByText(LOGIN_INVALID_PASSWORD);
+
+    public Locator logoutMessage = page.getByText(LOGOUT_MESSAGE);
+
+    @Step("Fill userName input with {username}")
+    public void fillUserName(String username){
+        usernameInput().fill(username);
     }
 
-    public Locator successMessage() {
-        return page.locator("b:has-text('Successfully registered, you can log in now.')");
+    @Step("Fill password")
+    public void fillPassword(String password){
+        passwordInput().fill(password);
     }
 
-    public String loginPageTitle() {
-        return page.locator("h1").textContent().trim();
+    @Step("Click 'Login' button")
+    public SecurePage clickLogin(){
+        loginButton().click();
+        page.waitForURL("**" + SECURE_PAGE);
+        return new SecurePage(page);
     }
 
-    public Locator usernameInvalidMessage() {
-        return page.locator("b:has-text('Your username is invalid')");
-    }
-
-    public Locator passwordInvalidMessage() {
-        return page.locator("b:has-text('Your password is invalid')");
-    }
-
-    @Step("Login user: {user}")
-    public LoginPage login(PracticeUiUser user) {
-        usernameField().fill(user.name());
-        passwordField().fill(user.password());
+    @Step("Click 'Login' button (expecting validation error)")
+    public LoginPage clickLoginExpectingFailure() {
         loginButton().click();
         return this;
+    }
+
+    public SecurePage fillLoginForm(PracticeUiUser user){
+        fillUserName(user.getUserName());
+        fillPassword(user.getPassword());
+        return clickLogin();
+    }
+
+    public LoginPage fillLoginFormExpectingFailure(PracticeUiUser user){
+        fillUserName(user.getUserName());
+        fillPassword(user.getPassword());
+        return clickLoginExpectingFailure();
+    }
+
+    @Step("Assert that Login Page is opened")
+    public void loginPageShouldBeOpened() {
+        page.waitForURL("**" + LOGIN_PAGE);
+
+        page.getByRole(AriaRole.HEADING,
+                        new Page.GetByRoleOptions().setName("Test Login Page"))
+                .waitFor();
     }
 }
