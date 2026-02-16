9df497fc253d3fa45e517d0b8a562fb39271358a
Hanna.Shestsiuk <hannashestsiuk@gmail.com>
2026-02-16 16:27:16 +0100

    fix - remove unused import on Register Page

--- src/main/java/pages/practice_pages/RegisterPage.java
+++ src/main/java/pages/practice_pages/RegisterPage.java
@@ -4,7 +4,6 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.WaitForSelectorState;
-import helpers.VisibleLocator;
 import io.qameta.allure.Step;
 import pages.BasePage;
 import records.practice_records.PracticeUiUser;
