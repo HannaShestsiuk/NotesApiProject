c5a361478dddf7bcfc18d6387086352a7e1c72ba
Hanna.Shestsiuk <hannashestsiuk@gmail.com>
2026-02-17 18:31:00 +0100

    feat - Add DynamicTableTest

---
+++ src/test/java/ui_tests/practice/DynamicTableTest.java
@@ -0,0 +1,17 @@
+package ui_tests.practice;
+
+import org.junit.jupiter.api.Test;
+import pages.practice_pages.DynamicTablePage;
+import pages.practice_pages.HomePage;
+import ui_tests.BaseTest;
+
+public class DynamicTableTest extends BaseTest {
+
+    @Test
+    void validateChromeCpuUsage() {
+        HomePage home = new HomePage(page()).open();
+        DynamicTablePage dynamicTablePage = home.goToDynamicTablePage();
+
+        dynamicTablePage.compareCpuValues();
+    }
+}
